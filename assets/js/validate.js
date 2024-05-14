f9ea30265aabee1bc4e432ab1d5a3462ef44f317
M3 <m3devbox@gmail.com>
2024-05-14 13:56:49 -0500

    Add email validation via validate.js

---
+++ assets/js/validate.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    const email = document.getElementById('email').value;
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert('Please enter a valid email address.');
+        return false;
+    }
+    return true;
+}
